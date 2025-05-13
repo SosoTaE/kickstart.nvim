a750d241efe6a1926e3c961ed0d1d2fcb9c611ec
sosotae <sosotae1615@gmail.com>
2025-05-13 15:26:25 +0400

    custom plugins

--- init.lua
+++ init.lua
@@ -276,6 +276,9 @@ require('lazy').setup({
     },
   },
 
+  { import = 'custom.plugins' }, -- <<< ADD THIS LINE
+
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
